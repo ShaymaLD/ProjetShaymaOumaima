ee52c8b45e6d8515e0319dbbaa5c33de6b5e22a9
Shayma Lassoued <shaymalassoued99@gamil.com>
2022-11-18 20:39:12 +0100

    projet shayma oumayma

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { ContactComponent } from './contact/contact.component';
+import { ErreurComponent } from './erreur/erreur.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccueilComponent,
+    ProduitsComponent,
+    ContactComponent,
+    ErreurComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
